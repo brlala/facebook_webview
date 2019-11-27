55d69e6f4f92a5260977112845771d388bc53291
liheng <liheng@pand.ai>
2019-11-27 22:50:40 +0800

    completed profile route

--- client/src/components/underwriting/Underwriting.js
+++ client/src/components/underwriting/Underwriting.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import {getCurrentProfile} from "../../actions/profile";
 
-const Underwriting = props =>{
+const Underwriting = ({getCurrentProfile, auth, profile}) =>{
+  useEffect(()=>{
+    getCurrentProfile()
+  }, [])
   return(
     <div>
       Underwriting Category
@@ -10,6 +15,15 @@ const Underwriting = props =>{
 }
 
 Underwriting.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 }
 
-export default Underwriting
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+})
+
+export default connect(mapStateToProps, { getCurrentProfile }) (Underwriting)
