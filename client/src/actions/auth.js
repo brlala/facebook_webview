122aa76eda922597ce62eaa3fc37acf044c56c34
liheng <liheng@pand.ai>
2019-11-27 15:43:23 +0800

    loaded user entry into state

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,7 +3,30 @@ import { setAlert } from './alert'
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
 } from './types'
+import setAuthToken from '../utils/setAuthToken'
+
+// Load User
+export const loadUser = () => async dispatch => {
+  if(localStorage.token){
+    setAuthToken(localStorage.token)
+  }
+
+  try{
+    const res = await axios.get('/api/auth')
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    })
+  } catch(e){
+    dispatch({
+      type: AUTH_ERROR
+    })
+  }
+}
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -28,8 +51,10 @@ export const register = ({ name, email, password }) => async dispatch => {
     if (errors) {
       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
     }
+
+    dispatch({
+      type: REGISTER_FAIL,
+    })
   }
-  dispatch({
-    type: REGISTER_FAIL,
-  })
+
 }
