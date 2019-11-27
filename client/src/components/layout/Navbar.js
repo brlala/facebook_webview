1a316ccf256f4039817b972e6822f98cb1d8cbfc
liheng <liheng@pand.ai>
2019-11-27 16:05:36 +0800

    added login page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,19 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <Fragment>
-      Navbar page
-    </Fragment>
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to='/'>
+          <i className="fas fa-code"/> GELM Webview Microapp</Link>
+      </h1>
+      <ul>
+        <li><Link to="!#">Developers</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
   )
 }
 
