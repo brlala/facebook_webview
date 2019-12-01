939c5ce0f5f1d80d03e22728c412fe93fab095bf
liheng <liheng@pand.ai>
2019-12-01 17:56:59 +0800

    adding event listener

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-// ReactDOM.render(<App/>, document.getElementById('root'))
-
 document.getElementById("root").addEventListener("fb_msg_event", () => {
     ReactDOM.render(<App />, document.getElementById("root"));
 });
