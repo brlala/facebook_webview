6c5fbec5652e8283c81f24ae3c77e6eed0b9da01
liheng <liheng@pand.ai>
2019-12-01 17:07:53 +0800

    use constant window.fbpayload

--- client/src/App.js
+++ client/src/App.js
@@ -23,27 +23,26 @@ if (localStorage.token) {
 }
 
 const App = () => {
-  // window.fbPayload = {
-  //   "tid": "2114834805260152",
-  //   "thread_type": "USER_TO_PAGE",
-  //   "psid": "2114834805260152",
-  //   "signed_request": "DnCN94XCQ3dTXJg6FJGBguJFtwhEcTw51bumAeCmQc4.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvbW11bml0eV9pZCI6bnVsbCwiaXNzdWVkX2F0IjoxNTc1MTczNjUwLCJtZXRhZGF0YSI6bnVsbCwicGFnZV9pZCI6MzQ1OTA2MjE5MzgzNTcyLCJwc2lkIjoiMjExNDgzNDgwNTI2MDE1MiIsInRocmVhZF9wYXJ0aWNpcGFudF9pZHMiOm51bGwsInRocmVhZF90eXBlIjoiVVNFUl9UT19QQUdFIiwidGlkIjoiMjExNDgzNDgwNTI2MDE1MiJ9",
-  //   "metadata": null
-  // }
-  // console.log(window.fbPayload)
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      console.log('This will run after 2 second!')
-      console.log(window.fbPayload)
-      store.dispatch(facebookLogin(window.fbPayload))
-      store.dispatch(loadUser())
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
+  window.fbPayload = {
+    "tid": "2114834805260152",
+    "thread_type": "USER_TO_PAGE",
+    "psid": "2114834805260152",
+    "signed_request": "DnCN94XCQ3dTXJg6FJGBguJFtwhEcTw51bumAeCmQc4.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvbW11bml0eV9pZCI6bnVsbCwiaXNzdWVkX2F0IjoxNTc1MTczNjUwLCJtZXRhZGF0YSI6bnVsbCwicGFnZV9pZCI6MzQ1OTA2MjE5MzgzNTcyLCJwc2lkIjoiMjExNDgzNDgwNTI2MDE1MiIsInRocmVhZF9wYXJ0aWNpcGFudF9pZHMiOm51bGwsInRocmVhZF90eXBlIjoiVVNFUl9UT19QQUdFIiwidGlkIjoiMjExNDgzNDgwNTI2MDE1MiJ9",
+    "metadata": null
+  }
+  console.log(window.fbPayload)
   // useEffect(() => {
-  //   store.dispatch(facebookLogin(window.fbPayload))
-  //   store.dispatch(loadUser());
-  // }, [])
+  //   const timer = setTimeout(() => {
+  //     console.log('This will run after 2 second!')
+  //     store.dispatch(facebookLogin(window.fbPayload))
+  //     store.dispatch(loadUser())
+  //   }, 2000);
+  //   return () => clearTimeout(timer);
+  // }, []);
+  useEffect(() => {
+    store.dispatch(facebookLogin(window.fbPayload))
+    store.dispatch(loadUser());
+  }, [])
 
   return (
     <Provider store={store}>
