0027e6c880039ba7c73d9ea50ff4d8adc9c066d7
liheng <liheng@pand.ai>
2019-12-02 11:30:28 +0800

    making UI changes to underwriting app

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import Oops from './components/layout/Oops';
 
-document.getElementById("root").addEventListener("fb_msg_event_success", () => {
-    ReactDOM.render(<App />, document.getElementById("root"));
-});
-
-document.getElementById("root").addEventListener("fb_msg_event_error", () => {
-    ReactDOM.render(<Oops />, document.getElementById("root"));
-});
+// document.getElementById("root").addEventListener("fb_msg_event_success", () => {
+//     ReactDOM.render(<App />, document.getElementById("root"));
+// });
+//
+// document.getElementById("root").addEventListener("fb_msg_event_error", () => {
+//     ReactDOM.render(<Oops />, document.getElementById("root"));
+// });
+ReactDOM.render(<App />, document.getElementById("root"));
